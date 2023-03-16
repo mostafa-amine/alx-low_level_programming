311515b0003416e51e9871191f4b1a6660df7409
mostafa amine briere <90805670+mostafa-amine@users.noreply.github.com>
2023-03-16 18:41:40 +0000

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @void: void
+ * Return: 0
+*/
+int main(void)
+{
+printf("Size of a char: %i byte(s)\n", (int)sizeof(char));
+printf("Size of a int: %i byte(s)\n", (int)sizeof(int));
+printf("Size of a long int: %i byte(s)\n", (int)sizeof(long int));
+printf("Size of a long long int: %i byte(s)\n", (int)sizeof(long long int));
+printf("Size of a float: %i byte(s)\n", (int)sizeof(float));
+return (0);
+}
+
