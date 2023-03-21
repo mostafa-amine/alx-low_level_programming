b1ade625e2b08f3e43f0d42cbd410f1a70391477
mostafa-amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2023-03-21 14:51:04 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - lowercase alphabets.
+ * @void: void
+ * Return: list of alphabets
+*/
+void print_alphabet(void)
+{
+	int i = 0;
+
+	for (i = 97; i < 122; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
+
