c92f1ec89e16a10d8b40a24599b3f8ceb3426f53
mostafa-amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2023-03-21 14:58:22 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - lowercase alphabets.
+ * @void: void
+ * Return: list of alphabets
+*/
+void print_alphabet_x10(void)
+{
+	int i, j = 0;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+
+	_putchar('\n');
+}
+
