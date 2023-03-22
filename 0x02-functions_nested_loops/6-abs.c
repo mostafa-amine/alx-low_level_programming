f26dd4dfa03e045016a8384d2bc0062999df4f46
mostafa-amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2023-03-21 18:04:30 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * _abs - checks for lowercase character
+ * @n: int
+ * Return: int
+*/
+
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+
+}
+
