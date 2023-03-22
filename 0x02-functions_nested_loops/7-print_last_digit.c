20381c27d462a3bdb0ef4ad491cbc0597883e840
mostafa-amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2023-03-21 18:39:04 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+/**
+ * print_last_digit - print last digit
+ * @n: int
+ * Return: int
+*/
+
+int print_last_digit(int n)
+{
+
+	int last_digit;
+
+	if (n > 0 || n == 0)
+	{
+		last_digit = (n % 10);
+	}
+	else
+	{
+		last_digit = (-n % 10);
+	}
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+
+}
+
+
