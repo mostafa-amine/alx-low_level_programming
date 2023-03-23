76a1daa98e4c24012501408ec7d60b2c3995dd67
mostafa-amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2023-03-23 11:32:54 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+/**
+* main - prints the largest of 3 integers
+* Return: 0
+*/
+int main(void)
+{
+
+	int i = 0;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
+
