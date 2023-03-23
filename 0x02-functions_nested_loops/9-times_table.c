0971edbfdaae2f19333b6c3de334cdeeed811b3e
mostafa-amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2023-03-22 19:01:56 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -7,7 +6,42 @@
 */
 void times_table(void)
 {
-	printf("Hello");
+	int i = 0, j;
+	int u, x = 0;
+
+	while (i <= 9)
+	{
+		j = 0;
+		while (j <= 9)
+		{
+			u = (i * j) / 10;
+			x = (i * j) % 10;
+
+			if (u != 0)
+			{
+				_putchar(u + '0');
+				_putchar(x + '0');
+			}
+			else
+			{
+				if (j != 0)
+				{
+					_putchar(32);
+				}
+				_putchar(x + '0');
+			}
+
+			if (!(j == 9))
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
 
 
