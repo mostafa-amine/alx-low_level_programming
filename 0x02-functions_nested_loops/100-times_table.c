c22980fa71fd02554890ef072226da9bdf32b3e0
mostafa-amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2023-03-25 08:14:10 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,56 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table
- * @n: int
-*/
+ * print_times_table - prints the n times table, starting with 0
+ * @n: the table to print
+ *
+ * Return: void
+ */
 void print_times_table(int n)
 {
-	int i = 0, j, u, x;
+	int i, j, r;
 
-	if (!(n > 15 || n < 0))
+	if (n > 15 || n < 0)
+		return;
+
+	for (i = 0; i <= n; i++)
 	{
-		while (i <= n)
+		for (j = 0; j <= n; j++)
 		{
-			j = 0;
-			while (j <= n)
+			r = i * j;
+
+			if (j == 0)
+				_putchar('0');
+			else
 			{
-				u = (i * j) / 10;
-				x = (i * j) % 10;
-
-				if (u != 0)
-				{
-					_putchar(u + '0');
-					_putchar(x + '0');
-				}
-				else
-				{
-					if (j != 0)
-					{
-						_putchar(32);
-					}
-					_putchar(x + '0');
-				}
-
-				if (!(j == n))
+				if (r < 10)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(' ');
+					_putchar(r + '0');
 				}
-
-				j++;
+				else if (r < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((r / 10) + '0');
+					_putchar((r % 10) + '0');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((r / 100) + '0');
+					_putchar(((r / 10) % 10) + '0');
+					_putchar((r % 10) + '0');
+				}
 			}
-			_putchar('\n');
-			i++;
 		}
+
+		_putchar('\n');
 	}
 }
+
