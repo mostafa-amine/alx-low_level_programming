a7db6d50b5647b799257171f94dfad7e9d07f6ca
mostafa-amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2023-03-27 19:12:03 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,21 +6,21 @@
 */
 void rev_string(char *s)
 {
-	int i, length = 0;
+	int i = 0, j = 0, length = 0;
+	char temp;
 
-	while (*s != '\0')
+
+	while (s[i] != '\0')
 	{
-		length++;
-		s++;
+			i++;
+			length++;
 	}
 
-	for (i = 0; i < length; i++)
-	{
-		s--;
-		_putchar(*s);
+	for (i = 0, j = length - 1; i < j; i++, j--) {
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
 	}
-
-	_putchar('\n');
 
 }
 
