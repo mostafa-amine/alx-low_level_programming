7dd2981a13fa5c438c21124be7904bfa4053d8c4
mostafa-amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2023-03-30 09:20:40 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string.
+ * @s1: char
+ * @s2: char
+ * Return: diff
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, j = 0, diff;
+
+	while (s1[i] != '\0')
+	{
+		while (s2[j] != '\0')
+		{
+			if (s1[i] != s2[j])
+			{
+				diff = ((int) s1[i]) - ((int) s2[j]);
+				return (diff);
+			}
+			else
+			{
+				return (0);
+			}
+			j++;
+		}
+		i++;
+	}
+
+}
+
