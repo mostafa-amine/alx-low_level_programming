d003eb92f6b1e550ae457de6422921d6404a7181
mostafa-amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2023-03-30 17:33:40 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,10 +9,15 @@ char *cap_string(char *str)
 {
 	int i;
 
+	if (str[0] >= 'a' && str[0] <= 'z')
+	{
+		str[0] -= 32;
+	}
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (
-			str[i] == ' ' || str[i] == '\t' || str[i] == ','
+			str[i] == 0 || str[i] == ' ' || str[i] == '\t' || str[i] == ','
 			|| str[i] == '\n' || str[i] == ';' || str[i] == '.'
 			|| str[i] == '!' || str[i] == '?' || str[i] == '"'
 			|| str[i] == '(' || str[i] == ')' || str[i] == '{'
@@ -25,6 +30,8 @@ char *cap_string(char *str)
 			}
 		}
 	}
+
+
 
 	return (str);
 }
