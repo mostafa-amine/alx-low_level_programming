60e41fe5bbd16aebeb2a2632eb6319b8fda4e71a
mostafa-amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2023-04-03 09:14:51 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+/**
+ * _strchr -  function that locates a character in a string
+ * @s: char
+ * @c: char
+ * Return: the adress memory of the first occurrence else NULL returned
+*/
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+
+	return (NULL);
+}
+
