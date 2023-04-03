377825e034b94f9776c28ebb919a2c9fabcbbd3e
mostafa-amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2023-04-03 08:03:14 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+/**
+ * _memset - fills memory with a constant byte
+ * @s: char
+ * @b: char
+ * @n: unsigned int
+ * Return: s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+	for (i = 0; i < n; i++)
+	{
+		*s = b;
+		s++;
+	}
+
+	return s - n;
+}
+
