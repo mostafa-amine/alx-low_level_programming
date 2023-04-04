55f54ed1ce601c89884ee26a27e456c7e1e46aef
mostafa-amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2023-04-03 17:54:38 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,15 +11,18 @@
 char *_strchr(char *s, char c)
 {
 	unsigned int i;
+	char *chr;
+
+	chr = NULL;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			return (&s[i]);
+			chr = &s[i];
 		}
 	}
 
-	return (NULL);
+	return (chr);
 }
 
