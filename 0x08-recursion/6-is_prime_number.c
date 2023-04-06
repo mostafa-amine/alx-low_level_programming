ef9c920ddcc294342f4f4d7bfcb8f79e7fceb85a
mostafa-amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2023-04-05 23:52:54 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,33 @@
-#include "main.h"
 
+/**
+ * is_prime_number_helper - is_prime_number helper function
+ * @n: int
+ * @i: int
+ * @counter: int
+ * Return: 1 if n is prime otherwise 0
+*/
+int is_prime_number_helper(int n, int i, int counter)
+{
+	if (i > n)
+	{
+		if (counter == 2)
+		{
+			return (1);
+		}
+
+		else
+		{
+			return (0);
+		}
+
+	}
+	if (n % i == 0)
+	{
+		counter++;
+	}
+
+	return (is_prime_number_helper(n, i + 1, counter));
+}
 
 
 /**
@@ -9,22 +37,11 @@
 */
 int is_prime_number(int n)
 {
-	int i, counter = 0;
-
-	for (i = 1; i <= n; i++)
-	{
-		if (n % i == 0)
-		{
-			counter++;
-		}
-	}
-	if (counter == 2)
-	{
-		return (1);
-	}
-	else
+	if (n <= 1)
 	{
 		return (0);
 	}
+
+	return (is_prime_number_helper(n, 1, 0));
 }
 
