f73afed36e47cdff059fd74884e493938ebb2976
mostafa-amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2023-04-06 00:21:53 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+/**
+ * _print_rev_recursion - a function that prints a string in reverse
+ * @s: char
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	printf("%c", *s);
+}
+
