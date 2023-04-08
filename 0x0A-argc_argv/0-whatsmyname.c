501f62c6bf506435d2b53903c02983bd3f39c7a1
mostafa-amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2023-04-08 00:21:53 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
+
