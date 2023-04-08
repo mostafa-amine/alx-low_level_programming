146b3349aed8e6930a0d0c6aa68822c3a341158a
mostafa-amine <BRIERE.MOSTAFAAMINE@ofppt-edu.ma>
2023-04-08 00:45:30 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,17 +11,14 @@ int main(int argc, char *argv[])
 {
 	int n1, n2;
 
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[2]);
-
-	if (argc != 3)
+	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	else
 	{
-		printf("%d\n",  n1 * n2);
+		printf("%d\n",  atoi(argv[1]) * atoi(argv[2]));
 	}
 	return (0);
 }
